e4001fe8dba2a58d0243097f65ed2a28be7fa2d9
Arnav Patidar <arnavpatidar0503@gmail.com>
2025-04-08 06:28:11 +0530

    Third Commit: Navigation Bar Complete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true;
+    }
 }
 
 dependencies {
@@ -46,6 +51,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:25.1.3")
     implementation("com.intuit.sdp:sdp-android:1.1.1")
     implementation("com.intuit.ssp:ssp-android:1.1.1")
+    implementation ("com.github.Foysalofficial:NafisBottomNav:5.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/arnav/example/finmate/HomeActivity.java
+++ app/src/main/java/arnav/example/finmate/HomeActivity.java
@@ -1,27 +1,35 @@
 package arnav.example.finmate;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.nafis.bottomnavigation.NafisBottomNavigation;
+
+import arnav.example.finmate.fragments.AddFragment;
+import arnav.example.finmate.fragments.AnalysisFragment;
+import arnav.example.finmate.fragments.BlogFragment;
+import arnav.example.finmate.fragments.ChatFragment;
+import arnav.example.finmate.fragments.HomeFragment;
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 public class HomeActivity extends AppCompatActivity {
+    private static final int idHome = 1;
+    private static final int idAnalyses = 2;
+    private static final int idAdd = 3;
+    private static final int idBlog = 4;
+    private static final int idChatBot = 5;
+    NafisBottomNavigation navBar;
 
-    TextView textViewWelcome, textViewMessage;
-    Button buttonLogout;
 
-    FirebaseAuth mAuth;
-    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,41 +42,65 @@ public class HomeActivity extends AppCompatActivity {
             return insets;
         });
 
-        textViewWelcome = findViewById(R.id.textViewWelcome);
-        textViewMessage = findViewById(R.id.textViewMessage);
-        buttonLogout = findViewById(R.id.buttonLogout);
+        navBar = findViewById(R.id.navBar);
 
-        mAuth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
+        navBar.add(new NafisBottomNavigation.Model(idHome, R.drawable.home_icon));
+        navBar.add(new NafisBottomNavigation.Model(idAnalyses, R.drawable.analysis_icon));
+        navBar.add(new NafisBottomNavigation.Model(idAdd, R.drawable.add_icon));
+        navBar.add(new NafisBottomNavigation.Model(idBlog, R.drawable.blog_icon3));
+        navBar.add(new NafisBottomNavigation.Model(idChatBot, R.drawable.chat_bot_icon));
 
-        FirebaseUser user = mAuth.getCurrentUser();
+        loadFragment(new HomeFragment(), 0);
 
-        if (user != null) {
-            String uid = user.getUid();
+        navBar.setOnClickMenuListener(new Function1<NafisBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(NafisBottomNavigation.Model model) {
 
-            // Fetch user info from Firestore
-            db.collection("users").document(uid).get()
-                    .addOnSuccessListener(documentSnapshot -> {
-                        if (documentSnapshot.exists()) {
-                            String name = documentSnapshot.getString("name");
-                            String phone = documentSnapshot.getString("phone");
+                if (model.getId() == idHome) {
+                    loadFragment(new HomeFragment(),1);
+                } else if (model.getId() == idAnalyses) {
+                    loadFragment(new AnalysisFragment(),1);
+                } else if (model.getId() == idAdd) {
+                    loadFragment(new AddFragment(),1);
+                } else if (model.getId() == idBlog) {
+                    loadFragment(new BlogFragment(),1);
+                } else if (model.getId() == idChatBot) {
+                    loadFragment(new ChatFragment(),1);
+                }
 
-                            if (name != null && !name.isEmpty()) {
-                                textViewWelcome.setText("Welcome, " + name + "!");
-                            } else {
-                                textViewWelcome.setText("Welcome, " + phone + "!");
-                            }
-
-                            // Optionally set a custom message or task
-                            textViewMessage.setText("You're already registered. Ready to track expenses?");
-                        }
-                    });
-        }
-
-        buttonLogout.setOnClickListener(v -> {
-            mAuth.signOut();
-            startActivity(new Intent(HomeActivity.this, arnav.example.finmate.MainActivity.class));
-            finish();
+                return null;
+            }
         });
+
+//        navBar.setOnShowListener(new Function1<NafisBottomNavigation.Model, Unit>() {
+//            @Override
+//            public Unit invoke(NafisBottomNavigation.Model model) {
+//
+//                if (model.getId() == idHome) {
+//                    loadFragment(new HomeFragment(),1);
+//                } else if (model.getId() == idAnalyses) {
+//                    loadFragment(new AnalysisFragment(),1);
+//                } else if (model.getId() == idAdd) {
+//                    loadFragment(new AddFragment(),1);
+//                } else if (model.getId() == idBlog) {
+//                    loadFragment(new BlogFragment(),1);
+//                } else if (model.getId() == idChatBot) {
+//                    loadFragment(new ChatBotFragment(),1);
+//                }
+//                return null;
+//            }
+//        });
+    }
+
+    private void loadFragment(Fragment fragment, int flag) {
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if (flag == 0) {
+            ft.add(R.id.container, fragment);
+        } else {
+            ft.replace(R.id.container, fragment);
+        }
+        ft.commit();
     }
 }
