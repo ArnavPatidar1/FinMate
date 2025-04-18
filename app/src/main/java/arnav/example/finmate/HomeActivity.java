885094a00e36e44791edde50bf5a3eea79a0f4f2
Arnav Patidar <arnavpatidar0503@gmail.com>
2025-04-18 14:52:36 +0530

    18/04/25

--- app/src/main/java/arnav/example/finmate/HomeActivity.java
+++ app/src/main/java/arnav/example/finmate/HomeActivity.java
@@ -5,47 +5,35 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.nafis.bottomnavigation.NafisBottomNavigation;
 
-import arnav.example.finmate.fragments.AddFragment;
-import arnav.example.finmate.fragments.AnalysisFragment;
-import arnav.example.finmate.fragments.BlogFragment;
-import arnav.example.finmate.fragments.ChatFragment;
-import arnav.example.finmate.fragments.HomeFragment;
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
+import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
-    private static final int idHome = 1;
-    private static final int idAnalyses = 2;
-    private static final int idAdd = 3;
-    private static final int idBlog = 4;
-    private static final int idChatBot = 5;
-    NafisBottomNavigation navBar;
+    BottomNavigationView navBar;
     MaterialToolbar toolbar;
 
     private FirebaseFirestore db;
     private FirebaseAuth auth;
     TextView toolbarTitle;
-
+    NavController navController;
 
 
     @SuppressLint("SetTextI18n")
@@ -56,24 +44,26 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
+
+        hideSystemUI();
 
         auth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
 
         toolbarTitle = findViewById(R.id.toolbarTitle);
 
-        String uid = auth.getCurrentUser().getUid();
+        String uid = Objects.requireNonNull(auth.getCurrentUser()).getUid();
         db.collection("users").document(uid)
                 .get()
                 .addOnSuccessListener(documentSnapshot -> {
                     if (documentSnapshot.exists()) {
-                        String name = documentSnapshot.getString("name");
+                        String[] name = documentSnapshot.getString("name").split(" ");
                         // Use the name wherever needed
-                        toolbarTitle.setText("Hello " + name);
-                        Log.d("Username", "User name: " + name);
+                        toolbarTitle.setText("Hello " + name[0]);
+                        Log.d("Username", "User name: " + name[0]);
                     } else {
                         Log.d("Username", "No such document");
                     }
@@ -82,84 +72,75 @@ public class HomeActivity extends AppCompatActivity {
                     Log.e("Username", "Failed to fetch user name", e);
                 });
 
-
         navBar = findViewById(R.id.navBar);
-
-
-
-        navBar.add(new NafisBottomNavigation.Model(idHome, R.drawable.home_icon));
-        navBar.add(new NafisBottomNavigation.Model(idAnalyses, R.drawable.analysis_icon));
-        navBar.add(new NafisBottomNavigation.Model(idAdd, R.drawable.add_icon));
-        navBar.add(new NafisBottomNavigation.Model(idBlog, R.drawable.blog_icon3));
-        navBar.add(new NafisBottomNavigation.Model(idChatBot, R.drawable.chat_bot_icon));
-
-        loadFragment(new HomeFragment(), 0);
-
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-        navBar.setOnClickMenuListener(new Function1<NafisBottomNavigation.Model, Unit>() {
+        navBar.getMenu().getItem(2).setEnabled(false);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.container);
+        if (navHostFragment != null) {
+            navController = navHostFragment.getNavController();
+        } else {
+            throw new IllegalStateException("NavHostFragment not found");
+        }
+        navBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public Unit invoke(NafisBottomNavigation.Model model) {
-
-                if (model.getId() == idHome) {
-                    loadFragment(new HomeFragment(),1);
-                } else if (model.getId() == idAnalyses) {
-                    loadFragment(new AnalysisFragment(),1);
-                } else if (model.getId() == idAdd) {
-                    loadFragment(new AddFragment(),1);
-                } else if (model.getId() == idBlog) {
-                    loadFragment(new BlogFragment(),1);
-                } else if (model.getId() == idChatBot) {
-                    loadFragment(new ChatFragment(),1);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.home) {
+                    navController.navigate(R.id.homeFragment);
+                    return true;
+                } else if (itemId == R.id.analyze) {
+                    navController.navigate(R.id.analyzeFragment);
+                    return true;
+                } else if (itemId == R.id.blog) {
+                    navController.navigate(R.id.blogFragment);
+                    return true;
+                } else if (itemId == R.id.chat) {
+                    navController.navigate(R.id.chatFragment);
+                    return true;
                 }
-
-                return null;
+                return false;
             }
         });
 
-//        navBar.setOnShowListener(new Function1<NafisBottomNavigation.Model, Unit>() {
-//            @Override
-//            public Unit invoke(NafisBottomNavigation.Model model) {
-//
-//                if (model.getId() == idHome) {
-//                    loadFragment(new HomeFragment(),1);
-//                } else if (model.getId() == idAnalyses) {
-//                    loadFragment(new AnalysisFragment(),1);
-//                } else if (model.getId() == idAdd) {
-//                    loadFragment(new AddFragment(),1);
-//                } else if (model.getId() == idBlog) {
-//                    loadFragment(new BlogFragment(),1);
-//                } else if (model.getId() == idChatBot) {
-//                    loadFragment(new ChatBotFragment(),1);
-//                }
-//                return null;
-//            }
-//        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            navController.navigate(R.id.addFragment);
+        });
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
-    private void loadFragment(Fragment fragment, int flag) {
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        if (flag == 0) {
-            ft.add(R.id.container, fragment);
-        } else {
-            ft.replace(R.id.container, fragment);
-        }
-        ft.commit();
-    }
-
-    // 🔽 Step 4: Inflate the menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
     }
 
+    // Handle back press with NavController
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp() || super.onSupportNavigateUp();
+    }
 
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
 
-
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
 
 }
